80c836f95a53d524f303d36b9d0edaccca88630b
God-COdeN1 <aniartom86@gmail.com>
2023-04-24 17:24:22 +0300

    Added perimeter of a square and triangle. Minor changes in the code

--- homework/homework_1/homework_str.py
+++ homework/homework_1/homework_str.py
@@ -1,13 +1,15 @@
 import string
+print("Doing homework")
 x = 15
 y = 5.29
-def perimeter(side1, side2, side3):
-    return side1 + side2 + side3
+def perTriangle(sideTr):
+    return sideTr * 3
+def perQuadrant(sideQu1, sideQu2):
+    return sideQu1 * 2 + sideQu2 * 2
 while True:
-    print("Doing homework")
-    CheckNom1 = True
     print("Input 1 for checking homework number 1 \nInput 2 for checking homework number 2 \nInput 3 for checking homework number 3")
-    while CheckNom1:
+    print("Input 4 for checking homework number 4 \nInput 5 for checking homework number 5 \nInput 6 for checking homework number 6")
+    while True:
         YourNom = input("Enter num: ")
         if any(char in string.ascii_letters for char in YourNom):
             print("Must contain a digit from 1 to 3")
@@ -22,10 +24,18 @@ while True:
         elif int(YourNom) == 3:
             print(id(x), id(y), "- here are their places in memory")
         elif int(YourNom) == 4:
-            side1 = int(input("Write the numbers with ,: "))
-            side2 = int(input("Write the numbers with ,: "))
-            side3 = int(input("Write the numbers with ,: "))
-            print(perimeter(side1, side2, side3))
+            print("7 python reserve words")
+            print("if \nelse \nelif \nand \nor \nTrue \nFalse ")
             break
+        elif int(YourNom) == 5:
+            sideQua1 = int(input("The first side of the square: "))
+            sideQua2 = int(input("The second side of the square: "))
+            print(perQuadrant(sideQua1, sideQua2))
+            break
+        elif int(YourNom) == 6:
+            sideTr = int(input("Side of the triangle: "))
+            print(perTriangle(sideTr))
+            break
+
 
 
