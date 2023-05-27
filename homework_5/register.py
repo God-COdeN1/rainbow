e91e97a5b8b9769a3201b276cb36c1c323c5661b
God-COdeN1 <aniartom86@gmail.com>
2023-05-27 22:30:15 +0300

    hm 5

---
+++ homework_5/register.py
@@ -0,0 +1,24 @@
+base = {
+
+}
+while True:
+    i = input("1 для регістрації, 2 для входу ")
+    if i == "1":
+        login = input("Логін: ")
+        pasword = input("Пароль: ")
+        if login in base:
+            print("Користувач існує")
+        else:
+            base[login] = pasword
+            print("Регістрація успішна")
+            print(base)
+    elif i == "2":
+        login = input("Логін: ")
+        if login in base:
+            pasword = input("Пароль: ")
+            if pasword == base[login]:
+                print("Вхід успішний")
+            else:
+                print("Пароль не вірний")
+        else:
+            print("Добавте користувача")
